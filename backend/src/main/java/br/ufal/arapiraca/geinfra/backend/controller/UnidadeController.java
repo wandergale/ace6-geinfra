cb5d3bd58f66e58161246f8e5be0767d01f04a09
Lucas Barbosa Silva <blucas.oficial@gmail.com>
2024-10-21 22:41:39 -0300

    adding get methods to unity

--- backend/src/main/java/br/ufal/arapiraca/geinfra/backend/controller/UnidadeController.java
+++ backend/src/main/java/br/ufal/arapiraca/geinfra/backend/controller/UnidadeController.java
@@ -1,17 +1,21 @@
 package br.ufal.arapiraca.geinfra.backend.controller;
 
 import java.net.URI;
-
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.ufal.arapiraca.geinfra.backend.controller.dto.UnidadeDTO;
 import br.ufal.arapiraca.geinfra.backend.controller.form.UnidadeForm;
 import br.ufal.arapiraca.geinfra.backend.model.Unidade;
 import br.ufal.arapiraca.geinfra.backend.repository.UnidadeRepository;
@@ -32,5 +36,21 @@ public class UnidadeController {
 
         URI uri = uriBuilder.path("/unidade/{id}").buildAndExpand(unidade.getId()).toUri();
         return ResponseEntity.created(uri).body(unidade);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UnidadeDTO> detalharSolicitacao(@PathVariable Long id) {
+        Optional<Unidade> opt = unidadeRepository.findById(id);
+        if(opt.isPresent()){
+            return ResponseEntity.ok(new UnidadeDTO(opt.get()));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping
+    public List<UnidadeDTO> lista() {
+        List<Unidade> lista = unidadeRepository.findAll();
+        
+        return UnidadeDTO.converter(lista);
     }
 }
