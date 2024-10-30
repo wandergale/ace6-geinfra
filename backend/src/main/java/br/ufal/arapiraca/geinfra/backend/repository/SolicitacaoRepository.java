1c0a7381fdb3a572e68ee97aa20ccb087533d029
Lucas Barbosa Leite Silva <58981172+LucasBarbosaSilva@users.noreply.github.com>
2024-10-29 22:21:18 -0300

    Revert "Delete methods"

--- backend/src/main/java/br/ufal/arapiraca/geinfra/backend/repository/SolicitacaoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.ufal.arapiraca.geinfra.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.ufal.arapiraca.geinfra.backend.model.Solicitacao;
-
-public interface SolicitacaoRepository extends JpaRepository<Solicitacao, Long>{
-    
-}
--- backend/src/test/java/br/ufal/arapiraca/geinfra/backend/BackendApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package br.ufal.arapiraca.geinfra.backend;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BackendApplicationTests {
-
-}
