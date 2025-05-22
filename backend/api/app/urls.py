df7f4f3f9cd60164e3e91927b5255d446e3cf7e8
Wanderson notebook <wandersonlife7@gmail.com>
2025-05-21 23:24:00 -0300

    rotas de solicitacao e lista

--- backend/api/app/urls.py
+++ backend/api/app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("solicitacoes/", views.SolicitacaoCreateView.as_view(), name="criar-solicitacao"),
+    path("solicitacoes/listar/", views.SolicitacaoListView.as_view(), name="listar-solicitacoes"),
 ]
