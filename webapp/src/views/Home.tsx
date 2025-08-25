54b79e1c05ad329f474e7670f0932d565501d60f
Wanderson PSS <wandersonlife7@gmail.com>
2025-08-25 14:44:28 -0300

    add: home page header

--- webapp/src/views/Home.tsx
+++ webapp/src/views/Home.tsx
@@ -3,14 +3,23 @@ import { Link } from "react-router-dom";
 function Home() {
 
   return (
-    <div className="text-center">
-      <h1 className="text-5xl font-bold text-heading my-8">
-        GEINFRA
-      </h1>
+    <div className="min-h-screen gb-gray-50">
+      <header className="bg-ufal-blue shadow-md">
+        <div className="container mx-auto px-4 py-4 flex items-center justify-between">
+          <div className="flex items-center space-x-4">
+            <img 
+              src="/logo-ufal.png" 
+              alt="Logo UFAL" 
+              className="h-12 w-auto"
+            />
+            <div className="text-white">
+              <h2 className="text-lg font-semibold">Universidade Federal de Alagoas</h2>
+              <p className="text-sm opacity-90">Sistema GEINFRA</p>
+            </div>
+          </div>
+        </div>
+      </header>
 
-      <nav className="mt-10">
-        <Link to="/solicitations" className="underline">Solicitações</Link>
-      </nav>
     </div>
   )
 }
